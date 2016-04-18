6b3022b45599ef11e492d8de1bac415c93ce39c7
andresmachado <csantos.machado@gmail.com>
2016-04-18 16:12:10 -0300

    Fix some minor errors

--- job_board/urls.py
+++ job_board/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.job_list, name='index'),
     url(r'^vagas/rss/$', LatestJobs(), name='rss_feed'),
     url(r'^vagas/(?P<category>[a-zA-Z0-9_.-]+)/$', views.filter_by_category, name="filter_category"),
-    url(r'^vagas/adicionar-vaga/$', views.job_create, name='job_create'),
-    url(r'^vagas/(?P<slug>[\w-]+)/$', views.job_detail, name='job_detail'),
+    url(r'^vagas/adicionar-vaga$', views.job_create, name='job_create'),
+    url(r'^vagas/(?P<slug>[\w-]+)$', views.job_detail, name='job_detail'),
 ]
