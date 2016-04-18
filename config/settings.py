242c1085ff134aaa32565201a93d2d2801fd5b1f
andresmachado <csantos.machado@gmail.com>
2016-04-18 14:51:42 -0300

    Heroku updates

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import psycopg2
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
+import urlparse
     
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
