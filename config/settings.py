e637f0b33986aa168b65f3b38332ca50ced960d2
andresmachado <csantos.machado@gmail.com>
2016-04-19 16:28:22 -0300

    Configuring site to serve sitemap.xml

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", ".vagastirecife.com.br", 'vagastirecife.herokuapp.com']
-SITE_ID = 1
+SITE_ID = 2
 
 
 # Application definition
